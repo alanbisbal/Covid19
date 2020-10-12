f018ddabf2f4c5210b6fe5a5d4b231f8d35ae302
Alan Bisbal <chuwa_bisbal@live.com>
2020-10-11 21:26:38 -0300

    show user agregado

--- app/resources/issue.py
+++ app/resources/issue.py
@@ -1,13 +1,11 @@
 from flask import redirect, render_template, request, url_for
 from app.db import connection
 from app.models.issue import Issue
-from app import db
 
 # Public resources
 def index():
     conn = connection()
     issues = Issue.all(conn)
-
     return render_template("issue/index.html", issues=issues)
 
 
