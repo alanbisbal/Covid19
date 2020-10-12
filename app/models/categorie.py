8e2af4b1ccfd3d233216c9fa4dd50f641f993c21
Cristobal Santiago <santiago.cristobal.sc@gmail.com>
2020-10-12 15:17:40 -0300

    conflictos resueltos de categorie issue auth y home

--- app/models/categorie.py
+++ app/models/categorie.py
@@ -7,16 +7,6 @@ class Categorie(db.Model):
     id = db.Column(db.Integer,primary_key = True)
 
 
-<<<<<<< HEAD
-def __init__(self, data):
-    self.name = data['name']
-    db.session.commit()
-
-
-@classmethod
-def __str__(self):
-    return '<categories {}>'.format(self.name)
-=======
     def __init__(self, data):
         self.name = data['name']
         db.session.commit()
@@ -25,4 +15,3 @@ def __str__(self):
     @classmethod
     def __str__(self):
         return '<categories {}>'.format(self.name)
->>>>>>> f018ddabf2f4c5210b6fe5a5d4b231f8d35ae302
--- app/models/issue.py
+++ app/models/issue.py
@@ -1,28 +1,15 @@
-class Issue(object):
-    @classmethod
-    def all(cls, conn):
-        sql = "SELECT * FROM issues"
-
-        cursor = conn.cursor()
-        cursor.execute(sql)
-
-        return cursor.fetchall()
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-def __init__(self, data):
-    self.email = data['email']
-    self.description = data['description']
-    self.description = data['description']
-    self.category = data['category']
-    self.status_id = data['status_id']
-    db.session.commit()
+from app import db
+from flask import request
 
 
-@classmethod
-def __str__(self):
-    return '<issues {}>'.format(self.email)
-=======
+class Issue(db.Model):
+    ____tablename__ = 'issues'
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    category = db.Column(db.Integer)
+    status_id = db.Column(db.Integer)
+
     def __init__(self, data):
         self.email = data['email']
         self.description = data['description']
@@ -30,24 +17,8 @@ def __str__(self):
         self.category = data['category']
         self.status_id = data['status_id']
         db.session.commit()
-=======
-    @classmethod
-    def create(cls, conn, data):
-        sql = """
-            INSERT INTO issues (email, description, category_id, status_id)
-            VALUES (%s, %s, %s, %s)
-        """
->>>>>>> f018ddabf2f4c5210b6fe5a5d4b231f8d35ae302
 
-        cursor = conn.cursor()
-        cursor.execute(sql, list(data.values()))
-        conn.commit()
 
-<<<<<<< HEAD
     @classmethod
     def __str__(self):
         return '<issues {}>'.format(self.email)
->>>>>>> d615a0aba5e1824d2261b3bf51976f98a8e2c4dd
-=======
-        return True
->>>>>>> f018ddabf2f4c5210b6fe5a5d4b231f8d35ae302
