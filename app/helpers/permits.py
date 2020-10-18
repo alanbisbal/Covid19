eb5bcb279bdf38b4a98cca95e710ddddc71500b1
Cristobal Santiago <santiago.cristobal.sc@gmail.com>
2020-10-17 22:18:10 -0300

    Principio de funcion en el helper

--- app/helpers/permits.py
+++ app/helpers/permits.py
@@ -11,7 +11,6 @@ Contacte con un administrador.'
 #@profile_permissions('user_index', current_user)
 #def profile(user_id):
 
-
 def permits_enabled(permit):
     """recibe un nombre de permiso y un usuario, si el usuario no tiene el permiso lo redirige al welcome"""
 
@@ -25,7 +24,7 @@ def permits_enabled(permit):
                 for n in perm_array:
                     if name = "user_new":
                         if usr.rol = "Operador":
-
+                            #Mejorar
 
 
 
