0ec36110cd22536c6efc688efc724f14fa654338
Alan Bisbal <chuwa_bisbal@live.com>
2020-10-30 04:45:03 -0300

    CRUD centros implementado (requeriments updateado, bd tambien. ajustar)

--- app/models/centro.py
+++ app/models/centro.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from app.db import db
 
 from sqlalchemy import Table, Column, Integer, ForeignKey
-from app.models import tipo_centro, turno
+from app.models import tipo_centro
 
 class Centro(db.Model):
     __tablename__ = 'centros'
@@ -14,30 +14,54 @@ class Centro(db.Model):
     phone = db.Column(db.String(255), nullable=False)
     open = db.Column(db.Time(timezone=True),nullable=False)
     close = db.Column(db.Time(timezone=True),nullable=False)
-    municipio = db.Column(db.String(255), nullable=False)
+    municipio_id = db.Column(db.String(255), nullable=False)
     web = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     state =  db.Column(db.Boolean, nullable=False)
     protocol = db.Column(db.String(255), nullable=False)
     coordinates = db.Column(db.String(255), nullable=False)
-    type = db.Column(Integer, ForeignKey('tipo_centros.id'))
-    turnos = db.relationship("Turno",backref= "centros")#checkear el backref
+    type = db.Column(db.String(255), nullable=False)
+    #db.Column(Integer, ForeignKey('tipo_centros.id'))
+    turnos = db.Column(db.String(255), nullable=False)
+    #db.relationship("Turno",backref= "centros")#checkear el backref
 
     def __init__(self, data):
         self.name = data['name']
         self.address = data['address']
         self.phone = data['phone']
         self.open = data['open']
-        self.close = data['close']
-        self.municipio = data['municipio']
+        self.close =  data['close']
+        self.municipio_id = "1"
         self.web = data['web']
         self.email = data['email']
-        self.state = data['state']
+        self.state = 0
         self.protocol = data['protocol']
         self.coordinates = data['coordinates']
         self.type = data['type']
+        self.turnos = data['turnos']
         db.session.commit()
 
     @classmethod
     def __str__(self):
         return '<Centro {}>'.format(self.name)
+
+
+    def add(data):
+        db.session.add(Centro(data))
+        db.session.commit()
+
+
+    def all():
+        return db.session.query(Centro).all()
+
+    def with_id(data):
+        return db.session.query(Centro).get(data)
+
+    #arreglar update
+    def update(self,data):
+        db.session.commit()
+
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
