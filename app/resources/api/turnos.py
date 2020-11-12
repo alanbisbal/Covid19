3d1cca0f44e58ce13abac4e0ef0fba67efcf26ee
Yajaira quipusco <yajairaquipusco97@gmail.com>
2020-11-12 16:43:41 -0300

    api de turnos por post falta terminarlo

--- app/resources/api/turnos.py
+++ app/resources/api/turnos.py
@@ -1,13 +1,14 @@
 from app.models.turno import Turno
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort,Response
 from datetime import date
+import json
 
 
 def turno_list(id,fecha=date.today()):
     try:
      turno= Turno.with_id_fecha(id,fecha)    
     except:
-        abort(500)
+        return Response(status=500)
 
     data_turno = []
 
@@ -20,4 +21,28 @@ def turno_list(id,fecha=date.today()):
             "fecha": str(i.fecha)
         })
 
-    return jsonify(turnos=data_turno)
+    final = json.dumps({"turnos": data_turno}, indent=2, ensure_ascii=False)
+    return Response(final, mimetype='application/json')
+
+
+def turno_create(id):
+    try:
+        turno = Turno.with_id(id)
+    except:
+        return Response(status=500)
+
+    if not turno:
+        return Response(status=400)
+
+    turno_creado= {}
+
+    turno_creado={
+        "centro_id": str(turno.centro_id),
+            "email": turno.email,
+            "hora_inicio": str(turno.hora_inicio),
+            "hora_fin": str(turno.hora_fin),
+            "fecha": str(turno.fecha)
+         }
+
+    final = json.dumps({"atributos": turno_creado}, indent=2)
+    return Response(final, status=201)
