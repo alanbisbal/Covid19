96cbf13be4699a9aba6fe3161645f8ff960b6477
Alan Bisbal <chuwa_bisbal@live.com>
2020-11-14 18:11:35 -0300

    filtro de hoy y 2 prox dias fix

--- app/models/turno.py
+++ app/models/turno.py
@@ -75,14 +75,15 @@ class Turno(db.Model):
 
 
     def with_next_two_date(centro_id):
-        hoy = datetime.today()
+        hoy = datetime.today().replace(hour=0,minute=0,second=0,microsecond=0)
         en_dos_dias = datetime.today() + timedelta(days=2)
         return db.session.query(Turno).filter(Turno.centro_id==centro_id).filter(Turno.fecha.between(hoy,en_dos_dias)).order_by(Turno.fecha.asc(),Turno.hora_inicio.asc())
 
     def with_next_two():
-        hoy = datetime.today()
+        hoy = datetime.today().replace(hour=0,minute=0,second=0,microsecond=0)
+        print(hoy)
         en_dos_dias = datetime.today() + timedelta(days=2)
-        return db.session.query(Turno).filter(Turno.fecha.between(hoy,en_dos_dias)).order_by(Turno.fecha.asc(),Turno.hora_inicio.asc())    
+        return db.session.query(Turno).filter(Turno.fecha.between(hoy,en_dos_dias)).order_by(Turno.fecha.asc(),Turno.hora_inicio.asc())
 
 
     #'bloque' no sabemos bien como definirlo
