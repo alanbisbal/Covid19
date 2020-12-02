8b6ca288c9427eb0b7d93648cff761c7e664fcda
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-02 18:11:17 -0300

    identacion solucionada

--- app/resources/turno.py
+++ app/resources/turno.py
@@ -15,9 +15,9 @@ from datetime import datetime, time, timedelta, date
 
 
 def index(centro_id=None):
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
-    de ser así muestra el listado con los turnos de hoy,mañana y pasado,que estan paginados de acuerdo a 
+    de ser así muestra el listado con los turnos de hoy,mañana y pasado,que estan paginados de acuerdo a
     los elementos almacenados en la configuración .
     Estos turnos pueden ser o no de un centro en particular
 
@@ -57,10 +57,10 @@ def index(centro_id=None):
 
 
 def new(centro_id=None):
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así muestra el formulario para la creacion de un turno seleccionando previamente
-    la fecha para dicho turno a partir de un centro en particular o habiendo seleccionado un centro. 
+    la fecha para dicho turno a partir de un centro en particular o habiendo seleccionado un centro.
 
     """
     if not authenticated(session):
@@ -90,7 +90,7 @@ def new(centro_id=None):
 
 
 def create():
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así carga el formulario con los datos ingresados y crea el turno habiendo hecho previamente
     las validaciones correspondientes para la creación del mismo.
@@ -132,7 +132,7 @@ def create():
 
 
 def update(turno_id):
-     """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así a partir de un turno en particular muestra la información previamente cargada en
     la creación del mismo
@@ -155,7 +155,7 @@ def update(turno_id):
 
 
 def update_new():
-     """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así obtiene los nuevos datos cargados en el formulario y realiza la actualización del mismo
     realizando previamente las validaciones correspondientes a los datos ingresados.
@@ -166,43 +166,43 @@ def update_new():
     if not has_permit('turno_update'):
         flash("No posee permisos", "danger")
         return redirect(url_for("home"))
-    
+
     form = TurnoForm()
     turno = Turno.with_id(request.form['turno_id'])
     if not form.validate_on_submit() or not turno:
         flash("El tipo de dato ingresado es incorrecto", "danger")
         return redirect(request.referrer)
     hora = datetime.strptime(form.data['hora_inicio'], '%H:%M:%S')
-    
+
     if not (hora.minute == 00 or hora.minute == 30):
         flash("El horario debe finalizar con los minutos xx:00 o xx:30",
               "danger")
         return redirect(url_for('turno_index', centro_id=form.centro_id.data))
     turnos_disponibles = Turno.bloques_disponibles(form.data['centro_id'],
                                                    str(form.data['fecha']))
-    
+
     if str(form.data['hora_inicio']) not in turnos_disponibles:
         flash("Bloque de turno ocupado", "danger")
-    
+
     turno.update(form.data)
     flash("Actualización exitosa.", "success")
-    
+
     return redirect(url_for('turno_index', centro_id=turno.centro_id))
 
 
 def delete():
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así elimina el turno seleccionado.
 
     """
     if not authenticated(session):
         abort(401)
-    
+
     if not has_permit('turno_destroy'):
         flash("No posee permisos", "danger")
         return redirect(url_for("home"))
-   
+
     turno = Turno.with_id(request.form['turno_id'])
     turno.delete()
     flash("Eliminación exitosa.", "success")
@@ -211,7 +211,7 @@ def delete():
 
 
 def show(turno_id):
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así a partir de un turno en particular muestra los datos del mismo
 
@@ -227,7 +227,7 @@ def show(turno_id):
 
 
 def search(centro_id=None):
-    """ 
+    """
     Este método verifica si el usuario esta logueado y tiene permisos para estar en esa sección,
     de ser así pagina el listado de turnos de acuerdo a los elementos almacenados en la configuración,
     mostrando los turnos que coincidan con la opción de búsqueda seleccionada y/o ingresada.
@@ -236,17 +236,17 @@ def search(centro_id=None):
     """
     if not authenticated(session):
         abort(401)
-    
+
     if not has_permit('turno_index'):
         flash("No posee permisos", "danger")
         return redirect(url_for("home"))
 
     email = request.args.get("email")
     filter = request.args.get("filtro")
-    
+
     per_page = Config.getConfig().elementos
     page = request.args.get("page", 1, type=int)
-    
+
     centros = Centro.all()
     form = NewTurnoForm()
     form.centro_id.choices = [(e.id, e.nombre) for e in centros]
@@ -258,11 +258,11 @@ def search(centro_id=None):
                                turnos=turnos,
                                centro_id=centro_id,
                                form=form)
-    
+
     centro = request.args.get("centro")
     search = SearchForm()
     search.centro.choices = [(e.nombre) for e in centros]
-    
+
     data = request.args
     if data['centro'] == "" and data['email'] == "":
         turnos = Turno.query.paginate(page, per_page, error_out=False)
@@ -284,7 +284,7 @@ def search(centro_id=None):
                                                           per_page,
                                                           error_out=False)
         return render_template("turno/index.html", turnos=turnos, form=form)
-    
+
     turnos = Turno.with_email_centro(data['email'],
                                      data['centro']).paginate(page,
                                                               per_page,
