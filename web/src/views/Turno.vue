182b9bc3eb71090cba30fc58bfef8a2e13458628
Yajaira quipusco <yajairaquipusco97@gmail.com>
2020-12-04 14:59:26 -0300

    formulario de turno con vue,botones del centro

---
+++ web/src/views/Turno.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <Turno/>
+  </div>
+
+
+</template>
+
+
+  <script>
+  import Turno from '@/components/Turno.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Turno
+    }
+  }
+  </script>
+
