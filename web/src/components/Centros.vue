972f3694dedadd17367bef9fd41953c2a1a5dcaa
Yajaira quipusco <yajairaquipusco97@gmail.com>
2020-12-04 15:38:05 -0300

    formulario un poco mas grande,segunda vez

--- web/src/components/Centros.vue
+++ web/src/components/Centros.vue
@@ -1,7 +1,17 @@
 <template>
   <div >
-    <p>
-    ACA VA EL MAPA CON LOS CENTROS
-    </p>
+    
+    <p>ACA VA EL MAPA CON LOS CENTROS; cuando selecciona reservar turno deberia primero elegir la fecha</p>
+    <div class="form-inline"> 
+    <div class="col-auto my-1">
+     <b-button variant="success" :to="{name:'centro'}">Crear Centro</b-button>
+     </div>
+     <div>
+      <b-button variant="primary" :to="{name:'turno'}">Reservar Turno</b-button>
+      </div>
+    </div>
   </div>
+
+
+
 </template>
---
+++ web/src/components/Turno.vue
@@ -0,0 +1,50 @@
+<template>
+<b-container fluid>
+  <div class="container col-md-8 col-sm-12">
+  <b-card-group deck >
+      <b-card bg-variant="ligth" header="Crear Turno" class="text-center" 
+      header-tag="header" style="max-width: 50rem;"  align="center">
+
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-small">Email:</label>
+    </b-col>
+    <b-col sm="10">
+      <b-form-input id="input-small" size="sm" type="email" placeholder="Ingrese su email"></b-form-input>
+    </b-col>
+     <b-col sm="2">
+      <label for="input-small">Telefono:</label>
+    </b-col>
+    <b-col sm="10">
+      <b-form-input id="input-small" size="sm" type="tel" placeholder="Ingrese su telefono"></b-form-input>
+    </b-col>
+     <b-col sm="2">
+      <label for="input-small">Hora de inicio:</label>
+    </b-col>
+    <b-col sm="10">
+      <b-form-input id="input-small" size="sm" type="time" placeholder="Ingrese la hora de inicio"></b-form-input>
+    </b-col>
+     <b-col sm="2">
+      <label for="input-small">Fecha:</label>
+    </b-col>
+    <b-col sm="10">
+      <b-form-input id="input-small" size="sm" type="date" readonly=true></b-form-input>
+    </b-col>
+  </b-row>
+  
+   
+   <div class="form-inline">
+          <div
+            class="card-body d-flex justify-content-between align-items-center"></div>
+          <div class="col-auto my-1">
+            <b-button variant="primary"  type="submit"> Crear</b-button>
+          </div>
+          
+        </div>
+
+  </b-card>
+  </b-card-group>
+
+  </div>
+</b-container>
+</template>
