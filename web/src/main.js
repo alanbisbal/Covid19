a7b5b89787a083841a2edd024c733206e20f5053
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-04 13:37:51 -0300

    componentes

--- web/src/main.js
+++ web/src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router' // Router being imported
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
