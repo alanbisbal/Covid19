bd0969d38d71853961d24b2253c956072f43bd44
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-04 15:57:39 -0300

    .

--- web/src/components/Centros.vue
+++ web/src/components/Centros.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
     <p>ACA VA EL MAPA CON LOS CENTROS; cuando selecciona reservar turno deberia primero elegir la fecha</p>
-    <div class="form-inline"> 
+    <div class="form-inline">
     <div class="col-auto my-1">
      <b-button variant="success" :to="{name:'centro'}">Crear Centro</b-button>
      </div>
