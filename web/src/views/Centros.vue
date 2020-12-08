1804dc190e05c2d89cee51a6154dfb47861ab950
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-08 19:44:21 -0300

    coordenadas de mapas implementado

--- web/src/views/Centros.vue
+++ web/src/views/Centros.vue
@@ -1,28 +1,19 @@
 <template>
   <div>
-    <myMap :centros="centros" />
+    <myMap />
   </div>
 </template>
 
 
   <script>
-  import axios from "axios";
+
   import myMap from '@/components/Maps/myMap.vue'
 
   export default {
     name: 'App',
     components: {
       myMap
-    },
-    data: function () {
-      return {
-        centros: []
-      }
-    },
-    mounted: function () {
-      axios.get("https://admin-grupo37.proyecto2020.linti.unlp.edu.ar/api/centros").then((result) => {
-        this.centros = result.data;
-      })
     }
+
   }
   </script>
