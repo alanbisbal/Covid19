56466ca59c9f055bce0d77a6a6a6bbf2d92842e2
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-15 15:40:27 -0300

    rama estadisticas

---
+++ web/src/components/Estadisticas/estadistica1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+    <p>
+    ACA VAN LAS ESTADISTICAS 1
+    </p>
+  </div>
+  <div >
+    <p>
+    ACA VAN LAS ESTADISTICAS 2
+    </p>
+  </div>
+  <div >
+    <p>
+    ACA VAN LAS ESTADISTICAS 3
+    </p>
+  </div>
+
+</template>
--- web/src/views/Estadisticas.vue
+++ web/src/views/Estadisticas.vue
@@ -1,12 +1,24 @@
 <template>
   <div
     class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm"
-  ></div>
+  >
+  <p>
+  ACA VAN LAS ESTADISTICAS 1
+  </p>
+  <p>
+  ACA VAN LAS ESTADISTICAS 2
+  </p>
+  <p>
+  ACA VAN LAS ESTADISTICAS 3
+  </p>
+
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: 'Estadisticas',
   components: {},
 };
 </script>
