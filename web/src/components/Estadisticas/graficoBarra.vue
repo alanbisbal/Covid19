765892b3f2645d320d73f90bfdf45960c40eba82
Alan Bisbal <chuwa_bisbal@live.com>
2020-12-19 06:55:38 -0300

    funcion dinamica de centro funcionando

--- web/src/components/Estadisticas/graficoBarra.vue
+++ web/src/components/Estadisticas/graficoBarra.vue
@@ -1,19 +1,23 @@
 <template>
   <div style="height: 500px; width: 100%" >
     <div class="col-md-4 col-sm-6">
-        <b-form  v-if="show"> Centros:
-        <select v-model="this.centro" v-bind='centrosPublicados' class="form-control" v-on:change='onInput'>
-                 <option v-for="centro in centrosPublicados" :key="centro.id" v-bind='centro' >
+        <b-form @reset="onReset" v-if="show"> Centros:
+        <select v-model="form.centro" class="form-control" v-on:change='onInput'>
+                 <option v-for="centro in centrosPublicados" :key="centro.id" v-bind:value='centro' >
                  {{ centro.nombre }}
                  </option>
              </select>
+
         <label class="mr-sm-2" for="inline-form-custom-select-pref"> Fecha:</label>
         <b-button type="reset" variant="danger">Limpiar</b-button>
         </b-form>
     </div>
 
-    <ve-bar :data="chartData" :settings="chartSettings">
-    </ve-bar>
+    <div class="col-md-8 col-sm-6">
+      <ve-bar :data="chartData" :settings="chartSettings">
+      </ve-bar>
+    </div>
+
   </div>
 </template>
 
@@ -32,17 +36,21 @@ import axios from 'axios';
           columns: [ 'mes', 'cantidad'],
           rows: [],
         },
-
-        centro:'19',
-        year: '2020',
+        form:{
+          centro:null,
+          year: '2020',
+        },
         result:null,
         show: true,
+        dataEmpty: true,
+        loading: true,
+
       }
     },
 
     mounted: function() {
     axios
-      .get('https://admin-grupo37.proyecto2020.linti.unlp.edu.ar/api/centros').
+      .get('http://127.0.0.1:5000/api/centros').
       then((result) => {
         this.centrosPublicados= result.data.centros;
       });
@@ -57,16 +65,40 @@ import axios from 'axios';
         });
     },
     methods: {
+
       async onInput(){
-      axios
-        .get('http://127.0.0.1:5000/api/centros/turnos_mes/19/2020')
+        if (this.form.centro && this.form.year) {
+         this.loading= true;
+         axios
+        .get('http://127.0.0.1:5000/api/centros/turnos_mes/'+this.form.centro.id +'/2020')
         .then((result) => {
           this.result = result.data;
           this.chartData.rows = this.result.data;
         });
+        setTimeout(() => { this.loading= false; }, 3000);
+         this.dataEmpty= false;
+       }
+     },
 
-    }
-  }}
+    onReset(evt) {
+           evt.preventDefault();
+           // Reset our form values
+          console.log('form full',this.form);
+           this.form.centro = null;
+           this.form.year = 2000;
+           this.dataEmpty= true;
+           // Trick to reset/clear native browser form validation state
+           this.show = false;
+           this.$nextTick(() => {
+             console.log('form vacio',this.form);
+             this.show = true;
+           });
+         },
+  },
+
+
+
+}
 
 
 </script>
